44498da0fa3514d7b554342084f1d5c15ee4cf1c
ashwinichavhan6291 <ashwinichavhan6291@gmail.com>
2024-10-19 12:58:29 +0530

    axios

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,4 +1,63 @@
+import { useState } from "react";
+import axios from "axios";
+
 const Login = () => {
-  return <div>Login</div>;
+  const [emailId, setEmailId] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        "http://localhost:3000/login",
+        {
+          emailId,
+          password,
+        },
+        { withCredentials: true }
+      );
+      console.log(res.data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  return (
+    <div className=" flex justify-center my-10 ">
+      <div className="card bg-base-content text-white w-96 shadow-xl ">
+        <div className="card-body">
+          <h2 className="card-title justify-center">Login</h2>
+          <div>
+            <label className="form-control w-full max-w-xs my-2">
+              <div className="label ">
+                <span className="label-text text-white">EmailId</span>
+              </div>
+              <input
+                type="text"
+                value={emailId}
+                className="input input-bordered w-full max-w-xs text-black"
+                onChange={(e) => setEmailId(e.target.value)}
+              />
+            </label>
+
+            <label className="form-control w-full max-w-xs my-2">
+              <div className="label ">
+                <span className="label-text text-white">Password</span>
+              </div>
+              <input
+                type="text"
+                value={password}
+                className="input input-bordered w-full max-w-xs text-black"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </label>
+          </div>
+          <div className="card-actions justify-center">
+            <button className="btn btn-primary " onClick={handleLogin}>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Login;
