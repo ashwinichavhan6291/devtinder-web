9c26ee031567770a9e999f48c35da74c419f14e0
ashwinichavhan6291 <ashwinichavhan6291@gmail.com>
2024-10-25 19:20:15 +0530

    Authentication of user and redirect to /

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -2,6 +2,5 @@ import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
 const appStore = configureStore({
   reducer: { user: userReducer },
-  // devTools: process.env.NODE_ENV !== "production",
 });
 export default appStore;
