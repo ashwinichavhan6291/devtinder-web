85c99eb29520c36572025188b0f481577ddcec5f
ashwinichavhan6291 <ashwinichavhan6291@gmail.com>
2024-10-25 20:47:37 +0530

    Logout features

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -8,6 +8,7 @@ import { BASE_URL } from "../utils/constants";
 const Login = () => {
   const [emailId, setEmailId] = useState("virat@gmail.com");
   const [password, setPassword] = useState("Virat@123");
+  const [error, setError] = useState("");
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -25,7 +26,7 @@ const Login = () => {
       dispatch(addUser(res.data));
       return navigate("/");
     } catch (err) {
-      console.error(err);
+      setError(err?.response?.data || "Something Went Wrong");
     }
   };
   return (
@@ -58,6 +59,7 @@ const Login = () => {
               />
             </label>
           </div>
+          <p className="text-red-500">{error}</p>
           <div className="card-actions justify-center">
             <button className="btn btn-primary " onClick={handleLogin}>
               Login
